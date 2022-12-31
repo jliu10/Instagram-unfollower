edae3eb390adaaa169ae30b1876969f5fcea0953
Justin Liu <exovitejl@gmail.com>
2022-12-30 22:58:27 -0500

    Check existence of accounts.txt

--- Instagram_unfollower.py
+++ Instagram_unfollower.py
@@ -2,3 +2,14 @@ from pathlib import Path
 
 # EDGE CASE: Account name changes after name was recorded, but before trying to
 #   unfollow
+# FEATURES:
+#   - Whitelist accounts to exempt them from being automatically unfollowed
+
+print("hello world")
+print(Path.cwd())
+
+accounts = Path.cwd() / "accounts.txt"
+if accounts.exists():
+    print(accounts, "is a valid path")
+else:
+    print(accounts, "is NOT a valid path")
