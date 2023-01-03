4117c10645b11473684460005ce1c9d168aeb797
Justin Liu <exovitejl@gmail.com>
2023-01-03 15:18:25 -0500

    Import selenium and add useful comments

--- Instagram_unfollower.py
+++ Instagram_unfollower.py
@@ -1,9 +1,23 @@
 from pathlib import Path
+import shelve, bs4, requests, lxml
+from selenium import webdriver
 
-# EDGE CASE: Account name changes after name was recorded, but before trying to
+# GOAL:
+#   - Log into desired Instagram account with user-inputted username and
+#       password
+#   - Access and collect list of Following
+#   - Access each Following account and see if it Follows Back
+#       - If not, then add to list of Guilty accounts (accounts.txt)
+#           - If CONFIRM is off, immediately Unfollow as well
+#   - Print list of Guilty accounts
+# EDGE CASES:
+#   - Account name changes after name was recorded, but before trying to
 #   unfollow
 # FEATURES:
-#   - Whitelist accounts to exempt them from being automatically unfollowed
+#   - WHITELIST: Whitelist accounts to exempt them from being automatically unfollowed
+#   - CONFIRM: Print list of Guilty accounts, then ask for confirmation to unfollow all
+#   - Toggle need for CONFIRM (unfollow without asking for confirmation, so
+#       user doesn't have to wait for script to get Guilty accounts)
 
 print("hello world")
 print(Path.cwd())
@@ -15,6 +29,32 @@ if not accounts.is_file():
 
 print(accounts, "is a valid file")
 
-print("Contents of accounts.txt: ", accounts.read_text())
-accounts.write_text("Rick roll")
-print("Contents of accounts.txt: ", accounts.read_text())
+##print("Contents of accounts.txt: ", accounts.read_text())
+##accounts.write_text("Rick roll")
+##print("Contents of accounts.txt: ", accounts.read_text())
+### Clear accounts.txt
+##accounts.write_text("")
+##print("Contents of accounts.txt: ", accounts.read_text())
+
+url = "https://www.instagram.com/jinsen2cold/"
+# Configure res to get webpage in specific manner
+headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0"}
+# Get user's insta page
+res = requests.get(url, headers=headers)
+# Check for error
+res.raise_for_status()
+
+soup = bs4.BeautifulSoup(res.text, "lxml")
+# Get user's "following"
+# v if logged in
+# following = soup.select("li.xl565be:nth-child(3) > a:nth-child(1) > div:nth-child(1)")
+# v if not logged in
+following = soup.select("li.xl565be:nth-child(3) > button:nth-child(1) > div:nth-child(1)")
+
+file = open(accounts)
+# open(accounts, 'w') for write mode
+# open(accounts, 'a') for append mode
+file.close()
+
+# Use shelve module for whitelisted accounts
+# To add to whitelist, prompt the user to input a single account name
